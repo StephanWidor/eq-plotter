d13a1a3ce214744351b2592d5bbfde2ab36f1ec3
Stephan Widor <stephan@widor.de>
2025-12-21 14:21:01 +0100

    tell slint explicitly to build native ui

--- eq-plotter-slint/build.rs
+++ eq-plotter-slint/build.rs
@@ -1,3 +1,4 @@
 fn main() {
-    slint_build::compile("ui/eq-plotter-ui.slint").expect("Slint build failed");
+    let config = slint_build::CompilerConfiguration::new().with_style("native".into());
+    slint_build::compile_with_config("ui/eq-plotter-ui.slint", config).unwrap();
 }
