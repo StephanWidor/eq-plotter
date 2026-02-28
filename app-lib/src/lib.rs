cacc0ff7b809ebb8a8f81824f27d792849ca6426
Stephan Widor <stephan@widor.de>
2026-02-28 18:06:20 +0100

    larger range for gains

--- app-lib/src/lib.rs
+++ app-lib/src/lib.rs
@@ -1,8 +1,8 @@
 use audio_lib::eq;
 
 pub const UI_BACKGROUND_COLOR: [u8; 3] = [32, 35, 38]; // [r,g,b]
-pub const MIN_GAIN_DB: f64 = -20.0;
-pub const MAX_GAIN_DB: f64 = 20.0;
+pub const MIN_GAIN_DB: f64 = -40.0;
+pub const MAX_GAIN_DB: f64 = 40.0;
 pub const MIN_FREQUENCY: f64 = 10.0;
 pub const MIN_LOG_FREQUENCY: f64 = 1.0; // 10.0.log10();
 pub const MAX_FREQUENCY: f64 = 20000.0;
