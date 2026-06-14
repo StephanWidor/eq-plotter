038dc55cd9ce0f039f26551309460dfee43ab001
Stephan Widor <stephan@widor.de>
2026-06-14 09:48:18 +0200

    we don't need pretty strings in persistence json

--- app-lib/src/persistence.rs
+++ app-lib/src/persistence.rs
@@ -12,7 +12,7 @@ pub fn create_from_json_file<T: serde::de::DeserializeOwned>(
 pub fn save_to_json_file<T: serde::Serialize>(t: &T, file_path: &std::path::Path) -> bool {
     if let Some(parent_dir) = file_path.parent() {
         if std::fs::create_dir_all(&parent_dir).is_ok() {
-            if let Ok(json) = serde_json::to_string_pretty(t) {
+            if let Ok(json) = serde_json::to_string(t) {
                 if std::fs::write(file_path, &json).is_ok() {
                     return true;
                 }
