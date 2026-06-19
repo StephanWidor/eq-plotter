4307a7093b960f12d597b02588473678ee3da537
Stephan Widor <stephan@widor.de>
2026-06-19 10:41:25 +0200

    move app_lib::Settings to eq-plugin

--- eq-plugin/src/lib.rs
+++ eq-plugin/src/lib.rs
@@ -3,6 +3,7 @@ pub mod params;
 pub mod plugin;
 pub mod presets;
 pub mod processing;
+pub mod settings;
 pub mod ui;
 
 use config::*;
@@ -11,7 +12,7 @@ pub use nice_plug::prelude as nice;
 pub type EqRanges = app_lib::settings::ui::EqRanges<f32>;
 pub type ImpulseResponseParams = app_lib::settings::ui::ImpulseResponseParams<f32>;
 pub type ShowOptions = app_lib::settings::ui::ShowOptions;
-pub type AppSettings<const NUM_BANDS: usize> = app_lib::settings::Settings<f32, NUM_BANDS>;
+pub type AppSettings<const NUM_BANDS: usize> = settings::Settings<f32, NUM_BANDS>;
 pub type Presets<const NUM_BANDS: usize> = presets::Presets<f32, NUM_BANDS>;
 pub type Plugin =
     plugin::Plugin<{ Config::NUM_BANDS }, { Config::NUM_CHANNELS }, { Config::ANALYZER_NUM_BINS }>;
